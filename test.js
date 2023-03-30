38cd88ad50038f7656d02f88f800576b7a08e14f
Rory Preddy <rorypreddy@gmail.com>
2023-03-30 16:07:17 +0200

    axe test example

---
+++ test.js
@@ -0,0 +1,17 @@
+const AxeBuilder = require('@axe-core/playwright').default;
+const playwright = require('playwright');
+
+(async () => {
+  const browser = await playwright.chromium.launch();
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  await page.goto('https://dequeuniversity.com/demo/mars/');
+
+  try {
+    const results = await new AxeBuilder({ page }).analyze();
+    console.log(results);
+  } catch (e) {
+    // do something with the error
+  }
+  await browser.close();
+})();
